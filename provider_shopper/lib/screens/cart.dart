6cd43f8afe6c20145bedcf8c1562d8b1016830ee
AlexPevnev <pan@12820.ru>
2021-03-17 09:57:03 +0500

    Downgrade to notNullSafery

--- provider_shopper/lib/screens/cart.dart
+++ provider_shopper/lib/screens/cart.dart
@@ -65,7 +65,7 @@ class _CartTotal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var hugeStyle =
-        Theme.of(context).textTheme.headline1!.copyWith(fontSize: 48);
+        Theme.of(context).textTheme.headline1.copyWith(fontSize: 48);
 
     return SizedBox(
       height: 200,
@@ -85,8 +85,8 @@ class _CartTotal extends StatelessWidget {
             SizedBox(width: 24),
             TextButton(
               onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Buying not supported yet.')));
+                // ScaffoldMessenger.of(context).showSnackBar(
+                //     SnackBar(content: Text('Buying not supported yet.')));
               },
               style: TextButton.styleFrom(primary: Colors.white),
               child: Text('BUY'),
